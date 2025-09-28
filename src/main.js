041764463c5757af074c6e3d0f587ef120eb738f
AH-THAN Itokina <151835739+Aadr1en@users.noreply.github.com>
2025-09-28 17:41:55 +0300

    Créer les fichiers de base manquants

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Pipelina</h1>
+    <div class="card">
+      <p>
+        Bienvenue dans Pipelina - Une application web moderne et simple.
+      </p>
+      <p>
+        Commencez à développer en modifiant <code>src/main.js</code>
+      </p>
+    </div>
+    <p class="read-the-docs">
+      Consultez la documentation pour en savoir plus
+    </p>
+  </div>
+`
